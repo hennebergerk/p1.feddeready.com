84e9c7a49cd51b175a9822b2ebb7c1f555cf16ed
Kevin Henneberger <henneberger@g.harvard.edu>
2013-09-12 21:58:59 -0400

    bio and code editor info added

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <h1>Welcome to P1</h1>
-
 <h2>Kevin Henneberger</h2>
 
-<p>more info here</p>
+<h3>Brief bio:</h3>
+<p>Kevin Henneberger has been working as a Web Production Specialist in Boston for the past four years. His main responsibilities consist of scheduling and posting new content to <a href="jsh.christianscience.com" target="_blank"> JSH-Online.com</a>, producing and moderating a weekly live audio chat, and managing the social media presence for JSH-Online.</p>
+
+<p>Kevin grew up in Dallas, Texas, and went to school at Texas A&amp;M University&mdash;where he earned a degree in Agricultural Communications and Journalism. He met his wife (from California) during an internship (in Boston) in the summer of 2007. They lived in Texas for about a year (where they got married), and made their way back to Boston in the Spring of 2009. They both enjoy Boston and plan to remain there for a few more years.</p>
+
+<p>Hobbies that Kevin enjoys include: hiking, swimming, kayaking, and bowling.</p>
+
+<h3>Code editor:</h3>
+<p>SublimeText 2</p>
