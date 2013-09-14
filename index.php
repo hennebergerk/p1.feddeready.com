c3379b0217fb0858bec07507f5993d7ab22b9e89
Kevin Henneberger <henneberger@g.harvard.edu>
2013-09-13 21:17:49 -0400

    HTML and CSS repairs

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head>
   <meta charset="utf-8">
   <title>Project 1</title>
-  <meta name="DWA15" content="Welcome to P1">
+  <meta name="projects" content="Welcome to P1">
   <link rel="stylesheet" href="css/style.css?v=1">
 </head>
 
@@ -12,32 +12,28 @@
     	<header>
             <h1>Welcome to P1</h1>
 
-	       <div id="core" class="clearfix">
-            <section id="left">
-            	<h2>Kevin Henneberger</h2>
-
+	    		<div id="core" class="clearfix">
+            		<section id="left">
+            			<h2>Kevin Henneberger</h2>
+        </header>
 <h3>Brief bio:</h3>
 <p>Kevin Henneberger has been working as a Web Production Specialist in Boston for the past four years. His main responsibilities consist of scheduling and posting new content to <a href="http://jsh.christianscience.com" target="_blank"> JSH-Online.com</a>, producing and moderating a weekly live audio chat, and managing the social media presence for JSH-Online.</p>
-
-
 <p>Kevin grew up in Dallas, Texas, and went to school at Texas A&amp;M University&mdash;where he earned a degree in Agricultural Communications and Journalism. He met his wife (from California) during an internship (in Boston) in the summer of 2007. They lived in Texas for about a year (where they got married), and made their way back to Boston in the Spring of 2009. They both enjoy Boston and plan to remain there for a few more years.</p>
-
-            </section>
+           			</section>
 
             <section id="right">
-                <p>Hobbies that Kevin enjoys include:
+                <p>Hobbies that Kevin enjoys include:</p>
                 	<ul>
                     <li>hiking</li>
                     <li>swimming</li>
                     <li>kayaking</li>
                 	<li>bowling</li>
                		</ul>
-            	</p>
             </section>
-        </div>
+        	</div>
         
         <footer>
-           <h3>Code editor: SublimeText 2</h3>
+           <p><b>Code editor:</b> SublimeText 2</p>
         </footer>
     </div>
 
