fd7396cdfd1dd45dca1b5063a10e020b7cbffa38
Kevin Henneberger <henneberger@g.harvard.edu>
2013-09-13 21:37:01 -0400

    index.php

--- index.php
+++ index.php
@@ -8,18 +8,17 @@
 
 <body>
 
-	    <div id="wrapper">
+	<div id="wrapper">
     	<header>
             <h1>Welcome to P1</h1>
-
-	    		<div id="core" class="clearfix">
-            		<section id="left">
-            			<h2>Kevin Henneberger</h2>
-        </header>
-<h3>Brief bio:</h3>
-<p>Kevin Henneberger has been working as a Web Production Specialist in Boston for the past four years. His main responsibilities consist of scheduling and posting new content to <a href="http://jsh.christianscience.com" target="_blank"> JSH-Online.com</a>, producing and moderating a weekly live audio chat, and managing the social media presence for JSH-Online.</p>
-<p>Kevin grew up in Dallas, Texas, and went to school at Texas A&amp;M University&mdash;where he earned a degree in Agricultural Communications and Journalism. He met his wife (from California) during an internship (in Boston) in the summer of 2007. They lived in Texas for about a year (where they got married), and made their way back to Boston in the Spring of 2009. They both enjoy Boston and plan to remain there for a few more years.</p>
-           			</section>
+			<h2>Kevin Henneberger</h2>
+			<h3>Brief bio:</h3>
+		</header>
+		<div id="core" class="clearfix">
+            <section id="left">
+				<p>Kevin Henneberger has been working as a Web Production Specialist in Boston for the past four years. His main responsibilities consist of scheduling and posting new content to <a href="http://jsh.christianscience.com" target="_blank"> JSH-Online.com</a>, producing and moderating a weekly live audio chat, and managing the social media presence for JSH-Online.</p>
+				<p>Kevin grew up in Dallas, Texas, and went to school at Texas A&amp;M University&mdash;where he earned a degree in Agricultural Communications and Journalism. He met his wife (from California) during an internship (in Boston) in the summer of 2007. They lived in Texas for about a year (where they got married), and made their way back to Boston in the Spring of 2009. They both enjoy Boston and plan to remain there for a few more years.</p>
+           	</section>
 
             <section id="right">
                 <p>Hobbies that Kevin enjoys include:</p>
@@ -30,12 +29,12 @@
                 	<li>bowling</li>
                		</ul>
             </section>
-        	</div>
+       </div>
         
         <footer>
            <p><b>Code editor:</b> SublimeText 2</p>
         </footer>
-    </div>
+	</div>    
 
 </body>
 </html>
